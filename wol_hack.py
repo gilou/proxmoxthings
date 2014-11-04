b13a4fb9b228938de884cbf1490f4d25723ef174
Gilles Pietri <contact+github@gilouweb.com>
2014-11-04 20:48:04 +0100

    Factorization with functions, comments

--- wol_hack.py
+++ wol_hack.py
@@ -7,24 +7,22 @@
 
 import subprocess, pcap, dpkt, binascii, re
 
-def exec_clean(commande):
-    return subprocess.check_output(commande).rstrip().split('\n')
+def exec_clean(command):
+    """ Simple wrapper to execute a command, and get the result as a list """ 
+    return subprocess.check_output(command).rstrip().split('\n')
 
-qm = exec_clean(['qm', 'list'])
-vm_macs = {}
-
-for ligne in qm[1:]:
-    vm = ligne.strip().partition(' ')[0]
-    config = exec_clean(['qm', 'config', vm])
-    for info in config:
-        if info.startswith('net'):
-            mac = re.sub('^net[0-9]+: [^=]+=([A-F0-9:]+),.+', '\g<1>', info)
-            vm_macs[mac] = vm
-
-#print(vm_macs)
-pc = pcap.pcap()
-
-pc.setfilter('udp port 9 and (udp[8:4] == 0xFFFFFFFF and udp[12:2] == 0xFFFF)')
+def vmlist():
+    """ Returns a dictionnary with each MACs as keys, and the VM as value """
+    qm = exec_clean(['qm', 'list'])
+    vm_macs = {}
+    for ligne in qm[1:]:
+        vm = ligne.strip().partition(' ')[0]
+        config = exec_clean(['qm', 'config', vm])
+        for info in config:
+            if info.startswith('net'):
+                mac = re.sub('^net[0-9]+: [^=]+=([A-F0-9:]+),.+', '\g<1>', info)
+                vm_macs[mac] = vm
+    return vm_macs
 
 def add_colons_to_mac( mac_addr ) :
     """This function accepts a 12 hex digit string and converts it to a colon separated string"""
@@ -34,16 +32,28 @@ def add_colons_to_mac( mac_addr ) :
     r = ":".join(s)		# I know this looks strange, refer to http://docs.python.org/library/stdtypes.html#sequence-types-str-unicode-list-tuple-bytearray-buffer-xrange
     return r
 
-for ts, pkt in pc:
-    a = dpkt.ethernet.Ethernet(pkt)
-    i = a.data
-    u = i.data
-    w = u.data
-    macd = add_colons_to_mac(binascii.hexlify(w[6:13])).upper()
-    if macd in vm_macs.keys():
-        print('Demarrage de ' + vm_macs[macd])
-        try:
-            r = subprocess.check_output(['qm', 'start', vm_macs[macd]])
-            print(r)
-        except subprocess.CalledProcessError:
-            print('Start failed, probably because VM is already running, I know, I could have checked, but well.')
+def start_scan():
+    pc = pcap.pcap()
+    pc.setfilter('udp port 9 and (udp[8:4] == 0xFFFFFFFF and udp[12:2] == 0xFFFF)')
+
+
+    for ts, pkt in pc:
+        a = dpkt.ethernet.Ethernet(pkt)
+        #IP
+        i = a.data
+        #UDP
+        u = i.data
+        #WoL
+        w = u.data
+        macd = add_colons_to_mac(binascii.hexlify(w[6:13])).upper()
+        if macd in vm_macs.keys():
+            print('Demarrage de ' + vm_macs[macd])
+            try:
+                r = subprocess.check_output(['qm', 'start', vm_macs[macd]])
+                print(r)
+            except subprocess.CalledProcessError:
+                print('Start failed, probably because VM is already running, I know, I could have checked, but well.')
+
+if __name__ == '__main__':
+    vm_macs = vmlist()
+    start_scan()
